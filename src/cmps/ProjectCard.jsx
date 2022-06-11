c9d96452f39be000ba9a815b3716e49fe847c554
SaharGar <ssahar456@gmail.com>
2022-06-11 19:52:33 +0300

    responsive design

--- src/cmps/ProjectCard.jsx
+++ src/cmps/ProjectCard.jsx
@@ -5,8 +5,8 @@ export const ProjectCard = ({ project }) => {
     <div className='project-card' style={{ backgroundImage: `url(${project.img})` }}>
       <div className="text-container flex column justify-center align-center">
         <h2 className='project-name'>{project.name}</h2>
-          <p className='project-desc'>{project.desc}</p>
-          <a className='project-link' href={project.url}>See Website</a>
+        <p className='project-desc'>{project.desc}</p>
+        <a className='project-link' href={project.url}>See Website</a>
       </div>
     </div>
   )
